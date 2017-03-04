e46b411233fc1fdad1c27b735a64b0ff68195ceb
carocch08 <caro.camposch@gmail.com>
2017-03-04 16:49:03 -0600

    Cambio en el HistogramEQ

--- HistogramEQ.php
+++ HistogramEQ.php
@@ -1,14 +1,14 @@
 <?php
 
-//function make_EQ()
-//{
+function make_EQ()
+{
 	$image = imagecreatefrompng('Ejemplo.png');
 	imagefilter($image, IMG_FILTER_GRAYSCALE);
 	//equalizeHist
 	
 	header('Content-Type: image/png');
 	imagepng($image, null, 9);
-//}
+}
 
-//make_EQ();
+make_EQ();
 ?>
