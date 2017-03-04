88e9dd04929ac886c2440db96a5895086f5772df
carocch08 <caro.camposch@gmail.com>
2017-03-04 10:09:30 -0600

    Modificación UploadImage

--- Convolucion.php
+++ Convolucion.php
@@ -9,5 +9,10 @@ function make_convolution($image, $filter, $pointX, $pointY)
 	header('Content-Type: image/png');
 	imagepng($imagen, null, 9);
 }
+
+//Llamada
+$ima = 'Ejemplo.png';
+$fil = array(array(2, 0, 0), array(0, -1, 0), array(0, 0, -1));
+make_convolution($ima, $fil, 1, 127);
 ?>
 
