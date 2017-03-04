a5ba24905a3b924468eb7626a045f3aaed0367e7
carocch08 <caro.camposch@gmail.com>
2017-03-04 13:00:02 -0600

    Casi funcional la Ecualización de histogramas

---
+++ HistogramEQ.php
@@ -0,0 +1,14 @@
+<?php
+
+//function make_EQ()
+//{
+	$image = imagecreatefrompng('Ejemplo.png');
+	imagefilter($image, IMG_FILTER_GRAYSCALE);
+
+	
+	header('Content-Type: image/png');
+	imagepng($image, null, 9);
+//}
+
+//make_EQ();
+?>
